29dba913fc73752356f24eecfd4860799777ea09
Sethmichael01 <49845849+Sethmichael01@users.noreply.github.com>
2020-04-15 23:00:36 +0100

    Explicit Loading for selected courses taught by an instructor

--- ContosoUniversity/Controllers/InstructorController.cs
+++ ContosoUniversity/Controllers/InstructorController.cs
@@ -35,8 +35,18 @@ namespace ContosoUniversity.Controllers
             if (courseID != null)
             {
                 ViewBag.CourseID = courseID.Value;
-                viewModel.Enrollments = viewModel.Courses.Where(
-                    x => x.CourseID == courseID).Single().Enrollments;
+                // Lazy loading
+                //viewModel.Enrollments = viewModel.Courses.Where(
+                //    x => x.CourseID == courseID).Single().Enrollments;
+                // Explicit loading
+                var selectedCourse = viewModel.Courses.Where(x => x.CourseID == courseID).Single();
+                db.Entry(selectedCourse).Collection(x => x.Enrollments).Load();
+                foreach (Enrollment enrollment in selectedCourse.Enrollments)
+                {
+                    db.Entry(enrollment).Reference(x => x.Student).Load();
+                }
+
+                viewModel.Enrollments = selectedCourse.Enrollments;
             }
 
             return View(viewModel);
