5a51971c22f9c745286219d26aa992ac46057bd8
Sethmichael01 <49845849+Sethmichael01@users.noreply.github.com>
2020-04-16 00:14:47 +0100

    Implemented Inheritance

--- ContosoUniversity/Models/Instructor.cs
+++ ContosoUniversity/Models/Instructor.cs
@@ -7,24 +7,12 @@ using System.Web;
 
 namespace ContosoUniversity.Models
 {
-    public class Instructor
+    public class Instructor:Person
     {
-        public int ID { get; set; }
-
-        [Display(Name = "Last Name"), StringLength(50, MinimumLength = 1)]
-        public string LastName { get; set; }
-
-        [Column("FirstName"), Display(Name = "First Name"), StringLength(50, MinimumLength = 1)]
-        public string FirstMidName { get; set; }
-
-        [DataType(DataType.Date), Display(Name = "Hire Date"), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Hire Date")]
         public DateTime HireDate { get; set; }
-
-        [Display(Name = "Full Name")]
-        public string FullName
-        {
-            get { return LastName + ", " + FirstMidName; }
-        }
 
         public virtual ICollection<Course> Courses { get; set; }
         public virtual OfficeAssignment OfficeAssignment { get; set; }
